7933dc45fc37977d8f9a4710afab45ab3f3bb893
arash <arash.bazaz@gmail.com>
2021-07-24 00:33:26 +0430

    addning creating post by admin

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,12 +1,15 @@
 from os import error
 import re
-from flask import session, render_template, request, abort, flash,redirect,url_for
+from sqlalchemy.exc import IntegrityError
+from flask import session, render_template, request, abort, flash, redirect, url_for
 from werkzeug.security import check_password_hash
 from . import admin
 from app import db
 from mod_users.models import User
 from mod_users.forms import Login_form
 from .utils import admin_only_viwe
+from mod_blog.forms import CreateBlog
+from mod_blog.models import Blog
 
 
 @admin.route('/')
@@ -14,35 +17,64 @@ from .utils import admin_only_viwe
 def index():
     return render_template('/admin/index.html')
 
-@admin.route('/login',methods=['POST','GET'])
+
+@admin.route('/login', methods=['POST', 'GET'])
 def login():
     form = Login_form(request.form)
     if request.method == 'POST':
-        if not form.validate_on_submit :
+        if not form.validate_on_submit:
             abort(400)
-        user = User.query.filter(User.email.ilike(f'{form.email.data}')).first()
-        if not user :
+        user = User.query.filter(
+            User.email.ilike(f'{form.email.data}')).first()
+        if not user:
             flash('incorrect credential', 'error')
-            return  render_template('admin/login_template.html',form = form)
+            return render_template('admin/login_template.html', form=form)
         if not user.check_password(form.password.data):
-            
+
             flash('incorrect credential', 'error')
-            return  render_template('admin/login_template.html',form = form)
+            return render_template('admin/login_template.html', form=form)
         if not user.is_user_admin():
             flash('you are not admin', 'error')
-            return  render_template('admin/login_template.html',form = form)
-        session['email']= user.email
-        session ['id'] =user.id
+            return render_template('admin/login_template.html', form=form)
+        session['email'] = user.email
+        session['id'] = user.id
         session['role'] = user.role
-        print (session)
+        print(session)
         return redirect(url_for('admin.index'))
-    if session.get('role') ==1:
+    if session.get('role') == 1:
         return redirect(url_for('admin.index'))
-    return render_template('admin/login_template.html',form = form)
+    return render_template('admin/login_template.html', form=form)
+
 
 @admin.route('/logout')
 @admin_only_viwe
 def logout():
     session.clear()
-    flash ('you logout successfully.', 'warning')
-    return redirect (url_for('admin.login'))
+    flash('you logout successfully.', 'warning')
+    return redirect(url_for('admin.login'))
+
+
+@admin.route('/post/new', methods=['POST', 'GET'])
+@admin_only_viwe
+def create_post():
+    form = CreateBlog(request.form)
+    if request.method == "POST":
+        if not form.validate_on_submit:
+            flash('insert required information')
+            return render_template('admin/add_new_post.html', form=form)
+        blog = Blog()
+        blog.title = form.title.data
+        blog.summery = form.summery.data
+        blog.content = form.content.data
+        blog.slug = form.slug.data
+
+        try:
+            db.session.add(blog)
+            db.session.commit()
+            flash('your blog submit sucsefully')
+            return redirect(url_for('admin.index'))
+        except IntegrityError:
+            db.session.rollback()
+            flash('there is a blog with this name. please change the name.')
+            return render_template('admin/add_new_post.html', form=form)
+    return render_template('admin/add_new_post.html', form=form)
--- mod_blog/models.py
+++ mod_blog/models.py
@@ -2,24 +2,29 @@ from sqlalchemy import Column, Integer, String, Text, Table, ForeignKey
 from app import db
 
 categori_blog = Table('category_blog', db.metadata,
-Column('id_category',Integer,ForeignKey('blog.id',ondelete='cascade')),
-Column('id_blog',Integer,ForeignKey('category.id', ondelete= 'cascade'))
-)
+                      Column('id_category', Integer, ForeignKey(
+                          'blog.id', ondelete='cascade')),
+                      Column('id_blog', Integer, ForeignKey(
+                          'category.id', ondelete='cascade'))
+                      )
 
 
 class Category(db.Model):
-    __tanlename__= 'category'
-    id =Column(Integer, primary_key=True)
-    name =Column(String(128),nullable= False, unique=True)
-    description = Column(String(256),nullable=True, unique= False)
-    slung =Column(String(128), nullable=False, unique= True)
-    blog = db.relationship("Blog",secondary=categori_blog,back_populates="category")
+    __tanlename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False, unique=True)
+    description = Column(String(256), nullable=True, unique=False)
+    slung = Column(String(128), nullable=False, unique=True)
+    blog = db.relationship("Blog", secondary=categori_blog,
+                           back_populates="category")
+
 
 class Blog(db.Model):
-    __tanlename__= 'blog'
-    id =Column(Integer, primary_key=True)
-    name =Column(String(128),nullable= False, unique=True)
-    description = Column(String(256),nullable=True, unique= False)
-    content= Column(Text, nullable= False, unique=True)
-    slung =Column(String(128), nullable=False, unique= True)
-    category  = db.relationship('Category',secondary=categori_blog,back_populates = 'blog')
+    __tanlename__ = 'blog'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128), nullable=False, unique=True)
+    summery = Column(String(256), nullable=True, unique=False)
+    content = Column(Text, nullable=False, unique=True)
+    slug = Column(String(128), nullable=False, unique=True)
+    category = db.relationship(
+        'Category', secondary=categori_blog, back_populates='blog')
